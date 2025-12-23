2a24d017dbf800a43254055931b456595a11a97d
Leonardo Palha <leonardo.palha@gmail.com>
2025-12-22 23:28:58 -0300

    fix: Add outputFileTracingIncludes for public route

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
+    },
+    outputFileTracingIncludes: {
+      '/(public)': ['**/*'],
     },
   },
   images: {
